453fa333cdb0cd2daef351119ab787084274f273
roommate6 <floginstefan@gmail.com>
2026-07-21 22:42:01 +0300

    +better description

--- js/roblox.js
+++ js/roblox.js
@@ -4,7 +4,7 @@ const projects = [
     title: "Farm with Squishy Dumplings",
     startDate: "2026-03",
     description:
-      "My second attempt of making a Roblox game. This is a Casual game I made with Lnor. Visit the game: https://www.roblox.com/games/134348150059910/Farm-with-Squishy-Dumplings",
+      "My second attempt of making a Roblox game.\n\nThis is a casual game were you collect dumplings and grow plants.\n\n## Dev team:\n\n- Lnor (Scripter)\n\n- Kaiser Dev (Builder / Modeler)\n\n- Roommate (UI)\n\nVisit the game:\n\nhttps://www.roblox.com/games/134348150059910/Farm-with-Squishy-Dumplings",
   },
   {
     pathToMediaFolder: "assets/media_packs/battle_quest",
